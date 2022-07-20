24c49ad638d8015eaa8de2066025d138e97c6ff9
HomeDev <HomeDev@DESKTOP-GU7MO6M>
2022-07-19 22:21:31 -0400

    Integração Spring Data JPA

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,28 +1,25 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		
-		pedido.setNomeProduto("Xiaomi Redmi Note 10S 128gb 6gb RAM");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/61pOZ7mAn2S._AC_SY450_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Xiaomi-Redmi-Note-10S-128gb/dp/B093FJZYZ5");
-		pedido.setDescricao("Uma descrição qualquer para esse pedido");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
 		model.addAttribute("pedidos", pedidos);
-		
 		return "home";
 	}
 }
